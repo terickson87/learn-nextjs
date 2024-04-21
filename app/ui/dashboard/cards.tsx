ef857b068ee5a7e2beebcb76816f1d212abbf6fb
Todd Ericskon <todd.erickson87@gmail.com>
2024-04-20 18:34:59 -0700

    Pagination Chapter 11 completed

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -29,11 +29,7 @@ export default async function CardWrapper() {
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      />
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
     </>
   );
 }
